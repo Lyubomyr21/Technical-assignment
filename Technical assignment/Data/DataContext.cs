e3f32b5e9bd5b1d4b26bd5741cf18eebd6efa8e6
Liubomyr <yupiter2101@gmail.com>
2022-09-11 06:07:31 +0300

    Implement the DataContext

--- Technical assignment/Data/DataContext.cs
+++ Technical assignment/Data/DataContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Technical_assignment.Models;
 
 namespace Technical_assignment.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<TestModel> testModels { get; set; }
     }
 }
