815ec1f2394862a737db80c10cc793e1ce11debb
Liubomyr <yupiter2101@gmail.com>
2022-09-11 05:22:37 +0300

    created base CRUD operations

--- Technical assignment/Controllers/TestController.cs
+++ Technical assignment/Controllers/TestController.cs
@@ -8,20 +8,71 @@ namespace Technical_assignment.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        private static List<TestModel> heroes = new List<TestModel>
+            {
+                new TestModel {
+                    Id = 1,
+                    Name ="Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                },
+                new TestModel {
+                    Id = 2,
+                    Name ="Ironman",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
+                }
+
+            };
+
+
         [HttpGet]
         public async Task<ActionResult<List<TestModel>>> Get()
         {
-            var heroes = new List<TestModel>
-            {
-                new TestModel { 
-                    Id = 1, 
-                    Name ="Spider Man", 
-                    FirstName = "Peter", 
-                    LastName = "Parker", 
-                    Place = "New York City"
-                }
-            };
+            return Ok(heroes);
+        }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TestModel>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Hero with id="+ id + " Not found");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<TestModel>>> AddHero(TestModel hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<TestModel>>> UpdateHero(TestModel request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+                return NotFound("Hero with id=" + request.Id + " Not found");
+            
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(heroes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TestModel>> Delete(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return NotFound("Hero with id=" + id + " Not found");
+
+            heroes.Remove(hero);
             return Ok(heroes);
         }
     }
