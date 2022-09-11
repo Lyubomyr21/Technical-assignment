b0be434849994deefc7308bd19565bc1bdd60df5
Liubomyr <yupiter2101@gmail.com>
2022-09-11 08:08:58 +0300

    implemented CRUD operations with DB

--- Technical assignment/Controllers/TestController.cs
+++ Technical assignment/Controllers/TestController.cs
@@ -10,24 +10,6 @@ namespace Technical_assignment.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        private static List<TestModel> heroes = new List<TestModel>
-            {
-                new TestModel {
-                    Id = 1,
-                    Name ="Spider Man",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York"
-                },
-                new TestModel {
-                    Id = 2,
-                    Name ="Ironman",
-                    FirstName = "Tony",
-                    LastName = "Stark",
-                    Place = "Long Island"
-                }
-            };
-
         private readonly DataContext _context;
 
         public TestController(DataContext context)
@@ -44,7 +26,7 @@ namespace Technical_assignment.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<TestModel>> Get(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.testModels.FindAsync(id);
             if (hero == null)
                 return NotFound("Hero with id="+ id + " Not found");
             return Ok(hero);
@@ -53,34 +35,41 @@ namespace Technical_assignment.Controllers
         [HttpPost]
         public async Task<ActionResult<List<TestModel>>> AddHero(TestModel hero)
         {
-            heroes.Add(hero);
-            return Ok(heroes);
+            _context.testModels.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.testModels.ToListAsync());
         }
 
         [HttpPut]
         public async Task<ActionResult<List<TestModel>>> UpdateHero(TestModel request)
         {
-            var hero = heroes.Find(h => h.Id == request.Id);
-            if (hero == null)
+            var dbHero = await _context.testModels.FindAsync(request.Id);
+            if (dbHero == null)
                 return NotFound("Hero with id=" + request.Id + " Not found");
-            
-            hero.Name = request.Name;
-            hero.FirstName = request.FirstName;
-            hero.LastName = request.LastName;
-            hero.Place = request.Place;
 
-            return Ok(heroes);
+            dbHero.Name = request.Name;
+            dbHero.FirstName = request.FirstName;
+            dbHero.LastName = request.LastName;
+            dbHero.Place = request.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.testModels.ToListAsync());
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<TestModel>> Delete(int id)
         {
-            var hero = heroes.Find(h => h.Id == id);
+            var hero = await _context.testModels.FindAsync(id);
             if (hero == null)
                 return NotFound("Hero with id=" + id + " Not found");
 
-            heroes.Remove(hero);
-            return Ok(heroes);
+            _context.testModels.Remove(hero);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.testModels.ToListAsync());
         }
     }
 }
