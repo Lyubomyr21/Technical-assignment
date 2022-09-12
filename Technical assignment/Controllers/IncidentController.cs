68e05ac2980f20e1428ef8de14952d070903af75
Liubomyr <yupiter2101@gmail.com>
2022-09-12 09:40:49 +0300

    create incident controller

---
+++ Technical assignment/Controllers/IncidentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Technical_assignment.Models;
+
+namespace Technical_assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public IncidentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("All")]
+        public async Task<ActionResult<List<Incident>>> Get()
+        {
+            return Ok(await _context.Incidents.ToListAsync());
+        }
+    }
+}
